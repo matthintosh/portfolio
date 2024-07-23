be333f7c86d5a0c5e30870f77f8d75273d62f0f7
Matthintosh <rocher.matthieu@gmail.com>
2024-07-23 14:05:36 +0200

    refactor: Improve bg with gradient instead of big image.

--- src/design/templates/PageTemplate.tsx
+++ src/design/templates/PageTemplate.tsx
@@ -2,10 +2,8 @@ import { PropsWithChildren } from 'react'
 
 export const PageTemplate = ({ children }: PropsWithChildren) => {
     return (
-        <main className="flex flex-col min-w-full min-h-screen bg-cover bg-main text-white bg-fixed">
-            <div className="flex-1 backdrop-blur-sm dark:backdrop-brightness-50">
-                {children}
-            </div>
+        <main className="flex flex-col min-w-full min-h-screen bg-cover text-white bg-fixed mainBackground">
+            <div className="flex-1 dark:backdrop-brightness-50">{children}</div>
         </main>
     )
 }
