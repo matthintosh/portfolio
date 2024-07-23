e619e074afcbf0e606e09bab693700b142b3b24a
Matthintosh <rocher.matthieu@gmail.com>
2024-07-23 14:45:06 +0200

    refactor: pretty

--- src/design/templates/PageTemplate.tsx
+++ src/design/templates/PageTemplate.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export const PageTemplate = ({ children }: PropsWithChildren) => {
     return (
-        <main className="flex flex-col min-w-full min-h-screen bg-cover text-white  mainBackground">
+        <main className="flex flex-col min-w-full min-h-screen bg-cover text-white mainBackground">
             <div className="flex-1 dark:backdrop-brightness-50 backdrop-blur-2xl">
                 {children}
             </div>
