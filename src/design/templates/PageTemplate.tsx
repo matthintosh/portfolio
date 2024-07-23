c0f9a1a9878f65aa5a94aefa16dbae3ad742ae33
Matthintosh <rocher.matthieu@gmail.com>
2024-07-23 14:44:51 +0200

    feature: Update background

--- src/design/templates/PageTemplate.tsx
+++ src/design/templates/PageTemplate.tsx
@@ -2,8 +2,10 @@ import { PropsWithChildren } from 'react'
 
 export const PageTemplate = ({ children }: PropsWithChildren) => {
     return (
-        <main className="flex flex-col min-w-full min-h-screen bg-cover text-white bg-fixed mainBackground">
-            <div className="flex-1 dark:backdrop-brightness-50">{children}</div>
+        <main className="flex flex-col min-w-full min-h-screen bg-cover text-white  mainBackground">
+            <div className="flex-1 dark:backdrop-brightness-50 backdrop-blur-2xl">
+                {children}
+            </div>
         </main>
     )
 }
